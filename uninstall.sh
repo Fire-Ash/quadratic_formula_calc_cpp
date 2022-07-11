0ffce45e8a2a0d16da53da930be6790cae5f201c
Ash <fire.ash9696@gmail.com>
2022-07-11 08:48:13 -0400

    forgot to indent and it was annoying me

--- uninstall.sh
+++ uninstall.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 main() {
-echo "Are you sure that you want to uninstall qf-calc? (y/n)"
-read response
-if [ "$response" = "y" ]; then
-  sudo rm /usr/bin/qf-calc
-elif [ "$response" = "n" ]; then
-  echo "exiting..."
-  exit 0
-else
-  echo "'$response' is not a valid option."
-  main
-fi
+  echo "Are you sure that you want to uninstall qf-calc? (y/n)"
+  read response
+  if [ "$response" = "y" ]; then
+    sudo rm /usr/bin/qf-calc
+  elif [ "$response" = "n" ]; then
+    echo "exiting..."
+    exit 0
+  else
+    echo "'$response' is not a valid option."
+    main
+  fi
 }
 
 main
