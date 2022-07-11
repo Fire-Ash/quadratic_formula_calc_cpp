b10ca044743d1945738ef33d196509bba1ef6003
Ash <fire.ash9696@gmail.com>
2022-07-11 08:27:50 -0400

    add comments

--- src/main.cpp
+++ src/main.cpp
@@ -20,22 +20,22 @@ int main(int argc, char *argv[])
         std::cout << "argument 1: value for a\nargument 2: value for b\nargument 3: value for c\n";
 
         return 1;
-    } else if(argc == 4) {
+    } else if(argc == 4) {        // set the arguments to the a, b and c values
         a = atof(argv[1]);
         b = atof(argv[2]);
         c = atof(argv[3]);
     }
 
-    double sqp1 = pow(b, 2);
-    double sqp2 = 4 * a * c;
-    double sq   = sqrt(sqp1 - sqp2);
-    double p2   = 2 * a;
+    double sqp1 = pow(b, 2);               // b^2
+    double sqp2 = 4 * a * c;                     // 4ac
+    double sq   = sqrt(sqp1 - sqp2);          // square root of the two things above
+    double p2   = 2 * a;                         // the thing at the bottom
 
-    double x1p1 = -b + sq;
-    double x2p1 = -b - sq;
+    double x1p1 = -b + sq;                       // -b + for x1
+    double x2p1 = -b - sq;                       // -b - for x2
 
-    double x1   = x1p1 / p2;
-    double x2   = x2p1 / p2;
+    double x1   = x1p1 / p2;                     // the top of the equation as x1p1 divided by the bottom
+    double x2   = x2p1 / p2;                     // the top of the equation as x2p1 divided by the bottom
 
     std::cout << "x1 = " << x1 << "\nx2 = " << x2 << "\n";
 
